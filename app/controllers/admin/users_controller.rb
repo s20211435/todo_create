d6eb9902a0d6c2b95cf7265ce17151d8f0a23cf9
s20211435 <yotamoti415717@gmail.com>
2025-04-06 12:10:34 +0000

    ユーザ機能作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :ensure_admin_user
 
   def index
-    @users = User.all
+    @users = User.all.order(:id)
   end
 
   def show
@@ -17,9 +17,12 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_path, notice: "ユーザーが作成されました。"
+      respond_to do |format|
+        format.html { redirect_to admin_users_path, notice: "ユーザーが作成されました。" }
+        format.turbo_stream
+      end
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -30,18 +33,21 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_user_path(@user), notice: "ユーザーが更新されました。"
+      respond_to do |format|
+        format.html { redirect_to admin_user_path(@user), notice: "ユーザーが更新されました。" }
+        format.turbo_stream
+      end
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @user = User.find(params[:id])
-    if @user.destroy
-      redirect_to admin_users_path, notice: "ユーザーが削除されました。"
-    else
-      redirect_to admin_users_path, alert: "ユーザーの削除に失敗しました。"
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_path, notice: "ユーザーが削除されました。" }
+      format.turbo_stream
     end
   end
 
@@ -51,5 +57,9 @@ class Admin::UsersController < ApplicationController
     unless current_user.admin?
       redirect_to root_path, alert: "管理者権限が必要です。"
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   end
 end
